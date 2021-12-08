5a92d34e466b514dcd54edb26689be7e22923352
Kevin Hisel <kevinhisel@gmail.com>
2021-12-08 01:58:11 -0600

    Implement find_pair to return lowest and highest priced days

--- stock_picker.rb
+++ stock_picker.rb
@@ -4,7 +4,6 @@ def find_index_of_lowest(array)
   index_of_lowest = nil
 
   array.each_with_index.reduce(array[0]) do |current_low, (possible_low, index)|
-    #binding.pry
     if current_low < possible_low
       current_low
     elsif index != array.length - 1
@@ -15,10 +14,32 @@ def find_index_of_lowest(array)
   index_of_lowest
 end
 
+def find_index_of_highest(array, offset = 0)
+  index_of_highest = nil
+
+  array.each_with_index.reduce(array[0]) do |current_high, (possible_high, index)|
+    if current_high > possible_high
+      current_high
+    else
+      index_of_highest = index + offset
+      possible_high
+    end
+  end
+  index_of_highest
+end
+
+def find_pair(array)
+  pair = Array.new(2)
+  pair[0] = find_index_of_lowest(array)
+  pair[1] = find_index_of_highest(array[pair[0]..array.length], pair[0])
+  pair
+end
+
 def stock_picker(prices)
-  find_index_of_lowest(prices)
+  find_pair(prices)
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
+p stock_picker([17,3,6,9,15,8,6,1])
 # => [1,4]  for a profit of $15 - $3 == $12
 # p stock_picker([17,3,6,9,15,8,6,10,1])
